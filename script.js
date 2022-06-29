c68d525111eadef1f505334e25521c9c82c6f1d3
Anna <annajesus.nave@gmail.com>
2022-06-29 17:39:39 -0300

    Implementando layout e js

---
+++ script.js
@@ -0,0 +1,257 @@
+//Comunicação com o servidor
+
+const SERVIDOR = "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes";
+const UNICO = "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/ID_DO_QUIZZ";
+const CRIAR = "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes"
+
+axios.get(SERVIDOR  ,   [
+	{
+		id: 1,
+		title: "Título do quizz",
+		image: "https://http.cat/411.jpg",
+		questions: [
+			{
+				title: "Título da pergunta 1",
+				color: "#123456",
+				answers: [
+					{
+						text: "Texto da resposta 1",
+						image: "https://http.cat/411.jpg",
+						isCorrectAnswer: true
+					},
+					{
+						text: "Texto da resposta 2",
+						image: "https://http.cat/412.jpg",
+						isCorrectAnswer: false
+					}
+				]
+			},
+			{
+				title: "Título da pergunta 2",
+				color: "#123456",
+				answers: [
+					{
+						text: "Texto da resposta 1",
+						image: "https://http.cat/411.jpg",
+						isCorrectAnswer: true
+					},
+					{
+						text: "Texto da resposta 2",
+						image: "https://http.cat/412.jpg",
+						isCorrectAnswer: false
+					}
+				]
+			},
+			{
+				title: "Título da pergunta 3",
+				color: "#123456",
+				answers: [
+					{
+						text: "Texto da resposta 1",
+						image: "https://http.cat/411.jpg",
+						isCorrectAnswer: true
+					},
+					{
+						text: "Texto da resposta 2",
+						image: "https://http.cat/412.jpg",
+						isCorrectAnswer: false
+					}
+				]
+			}
+		],
+		levels: [
+			{
+				title: "Título do nível 1",
+				image: "https://http.cat/411.jpg",
+				text: "Descrição do nível 1",
+				minValue: 0
+			},
+			{
+				title: "Título do nível 2",
+				image: "https://http.cat/412.jpg",
+				text: "Descrição do nível 2",
+				minValue: 50
+			}
+		]
+	}
+])
+
+
+
+//Para buscar um único quizz, substituindo `ID_DO_QUIZZ` pelo id do quizz desejado.
+axios.get(UNICO, {
+	id: 1,
+	title: "Título do quizz",
+	image: "https://http.cat/411.jpg",
+	questions: [
+		{
+			title: "Título da pergunta 1",
+			color: "#123456",
+			answers: [
+				{
+					text: "Texto da resposta 1",
+					image: "https://http.cat/411.jpg",
+					isCorrectAnswer: true
+				},
+				{
+					text: "Texto da resposta 2",
+					image: "https://http.cat/412.jpg",
+					isCorrectAnswer: false
+				}
+			]
+		},
+		{
+			title: "Título da pergunta 2",
+			color: "#123456",
+			answers: [
+				{
+					text: "Texto da resposta 1",
+					image: "https://http.cat/411.jpg",
+					isCorrectAnswer: true
+				},
+				{
+					text: "Texto da resposta 2",
+					image: "https://http.cat/412.jpg",
+					isCorrectAnswer: false
+				}
+			]
+		},
+		{
+			title: "Título da pergunta 3",
+			color: "#123456",
+			answers: [
+				{
+					text: "Texto da resposta 1",
+					image: "https://http.cat/411.jpg",
+					isCorrectAnswer: true
+				},
+				{
+					text: "Texto da resposta 2",
+					image: "https://http.cat/412.jpg",
+					isCorrectAnswer: false
+				}
+			]
+		}
+	],
+	levels: [
+		{
+			title: "Título do nível 1",
+			image: "https://http.cat/411.jpg",
+			text: "Descrição do nível 1",
+			minValue: 0
+		},
+		{
+			title: "Título do nível 2",
+			image: "https://http.cat/412.jpg",
+			text: "Descrição do nível 2",
+			minValue: 50
+		}
+	]
+})
+
+//Para criar quizz
+axios.post(CRIAR , {
+	title: "Título do quizz",
+	image: "https://http.cat/411.jpg",
+	questions: [
+		{
+			title: "Título da pergunta 1",
+			color: "#123456",
+			answers: [
+				{
+					text: "Texto da resposta 1",
+					image: "https://http.cat/411.jpg",
+					isCorrectAnswer: true
+				},
+				{
+					text: "Texto da resposta 2",
+					image: "https://http.cat/412.jpg",
+					isCorrectAnswer: false
+				}
+			]
+		},
+		{
+			title: "Título da pergunta 2",
+			color: "#123456",
+			answers: [
+				{
+					text: "Texto da resposta 1",
+					image: "https://http.cat/411.jpg",
+					isCorrectAnswer: true
+				},
+				{
+					text: "Texto da resposta 2",
+					image: "https://http.cat/412.jpg",
+					isCorrectAnswer: false
+				}
+			]
+		},
+		{
+			title: "Título da pergunta 3",
+			color: "#123456",
+			answers: [
+				{
+					text: "Texto da resposta 1",
+					image: "https://http.cat/411.jpg",
+					isCorrectAnswer: true
+				},
+				{
+					text: "Texto da resposta 2",
+					image: "https://http.cat/412.jpg",
+					isCorrectAnswer: false
+				}
+			]
+		}
+	],
+	levels: [
+		{
+			title: "Título do nível 1",
+			image: "https://http.cat/411.jpg",
+			text: "Descrição do nível 1",
+			minValue: 0
+		},
+		{
+			title: "Título do nível 2",
+			image: "https://http.cat/412.jpg",
+			text: "Descrição do nível 2",
+			minValue: 50
+		}
+	]
+})
+
+//Deve haver pelo menos 3 perguntas por quizz. Cada pergunta deve ter entre 2 e 4 respostas. Deve haver pelo menos 2 níveis por quizz.
+
+
+//JOGO
+
+let pontos = 0; //pontos para o placar
+let placar = 0; // placar
+
+
+
+//Criando telas com createElement()
+
+//Pergunta
+let numQuestao = document.querySelector('#numQuestao')
+let pergunta = document.querySelector('#pergunta')
+
+//Alternativas (são 4)
+let a = document.querySelector('#a')
+let b = document.querySelector('#b')
+let c = document.querySelector('#c')
+let d = document.querySelector('#d')
+
+
+
+
+
+function criarQuizz(){
+    // Se clicar no botão Criar Quizz : Aparece a conteúdo-escondido 
+
+}
+
+
+function verificarSeAcertou(){
+
+    
+}
